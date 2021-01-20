a09b8962857b6f60444c08768a440fde4ed87bf2
swopet <swopet@oregonstate.edu>
2021-01-19 20:20:12 -0800

    move styles

--- include/header.php
+++ include/header.php
@@ -3,7 +3,7 @@ $title = "Trevor's Cave" . $page_name;
 include($_SERVER['DOCUMENT_ROOT']."/../include/nav_bar.php")
 ?>
 <html>
-<link rel="stylesheet" href="/../include/styles.css">
+<link rel="stylesheet" href="styles.css">
 <head>
 <title><?php echo $title; ?></title>
 </head>
