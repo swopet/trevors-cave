827cde22dbea78b7669783f8b249ab15b4824167
swopet <swopet@oregonstate.edu>
2021-01-19 19:56:37 -0800

    echo

--- html/index.php
+++ html/index.php
@@ -1,10 +1,11 @@
 <?php
 $page_name = "Home";
+echo $_SERVER['DOCUMENT_ROOT'];
 include_once($_SERVER['DOCUMENT_ROOT']."/include/header.php");
 ?>
 
 <p>Welcome to Trevor's Cave (Under Construction)</p>
 
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/include/footer.php");
+include_once($_SERVER['DOCUMENT_ROOT']."/include/footer.php");
 ?>
