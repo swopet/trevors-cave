bdc738920444ba66cea31850a6f5f96da1128807
swopet <swopet@oregonstate.edu>
2021-01-19 19:50:36 -0800

    change ~ to /

--- html/index.php
+++ html/index.php
@@ -1,10 +1,10 @@
 <?php
 $page_name = "Home";
-include("~/include/header.php");
+include("/include/header.php");
 ?>
 
 <p>Welcome to Trevor's Cave (Under Construction)</p>
 
 <?php
-include("~/include/footer.php");
+include("/include/footer.php");
 ?>
