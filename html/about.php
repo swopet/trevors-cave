acdc4ec8d66bda269151d136a0babff87b1f8c85
swopet <swopet@oregonstate.edu>
2021-01-19 20:15:44 -0800

    doop doop

--- html/about.php
+++ html/about.php
@@ -1,10 +1,10 @@
 <?php
 $page_name = "About";
-include($_SERVER['DOCUMENT_ROOT']."/include/header.php");
+include($_SERVER['DOCUMENT_ROOT']."../include/header.php");
 ?>
 
 <p>This is my website!</p>
 
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/include/footer.php");
+include($_SERVER['DOCUMENT_ROOT']."../include/footer.php");
 ?>
--- include/header.php
+++ include/header.php
@@ -1,7 +1,9 @@
 <?php
 $title = "Trevor's Cave" . $page_name;
+include($_SERVER['DOCUMENT_ROOT']."../include/nav_bar.php")
 ?>
 <html>
+<link rel="stylesheet" href="/../include/styles.css">
 <head>
 <title><?php echo $title; ?></title>
 </head>
