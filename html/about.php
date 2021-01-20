b14df148439cbe257035b2441b90db5df8c0c075
swopet <swopet@oregonstate.edu>
2021-01-19 19:48:15 -0800

    change .. to ~

--- html/about.php
+++ html/about.php
@@ -1,10 +1,10 @@
 <?php
 $page_name = "About";
-include("../include/header.php");
+include("~/include/header.php");
 ?>
 
 <p>This is my website!</p>
 
 <?php
-include("../include/footer.php");
+include("~/include/footer.php");
 ?>
