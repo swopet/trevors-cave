432589f9d9199de1fc0815795ec2f5d0fd4010c5
Trevor Swope <trevor.m.swope@gmail.com>
2021-01-25 20:07:19 -0800

    update posts CSS formatting

--- posts/hex-shader-applet.php
+++ posts/hex-shader-applet.php
@@ -4,74 +4,76 @@
 <canvas id="glCanvas" width="0" height="0" class="shadercanvas"></canvas>
 </div>
 
-<div class="inputcontainer">
-<input type="file" id="file-selector" accept=".jpg, .jpeg, .png" class="fileselector"></input>
-<div style="text-align:center" id="widthxheight"></div>
-</div>
+<div class="postcontainer">
+	<div class="inputcontainer">
+	<input type="file" id="file-selector" accept=".jpg, .jpeg, .png" class="fileselector"></input>
+	<div style="text-align:center" id="widthxheight"></div>
+	</div>
 
-<?php 
-	function echoSlider($title,$id_base,$min,$max,$ratio,$units,$tooltiptext=null){
-		echo "<div class=\"inputcontainer\">";
-		echo "<div class=\"inputlabel\">".$title."</div>";
-		echo "<input type=\"range\" min=\"".$min*$ratio."\" max=\"".$max*$ratio."\" class=\"slider\" id=\"".$id_base."Input\">";
-		echo "<input type=\"text\" id=\"".$id_base."Display\" class=\"sliderdisplay\"> ".$units;
-		if ($tooltiptext!=null){
-			echo "<div class=\"tooltip\">?<span class=\"tooltiptext\">".$tooltiptext."</span></div>";
+	<?php 
+		function echoSlider($title,$id_base,$min,$max,$ratio,$units,$tooltiptext=null){
+			echo "<div class=\"inputcontainer\">";
+			echo "<div class=\"inputlabel\">".$title."</div>";
+			echo "<input type=\"range\" min=\"".$min*$ratio."\" max=\"".$max*$ratio."\" class=\"slider\" id=\"".$id_base."Input\">";
+			echo "<input type=\"text\" id=\"".$id_base."Display\" class=\"sliderdisplay\"> ".$units;
+			if ($tooltiptext!=null){
+				echo "<div class=\"tooltip\">?<span class=\"tooltiptext\">".$tooltiptext."</span></div>";
+			}
+			echo "</div>";
 		}
-		echo "</div>";
-	}
-	echoSlider("Minimum Side Length","minSize",0.1,50.0,10.0,"Pixels");
-	echoSlider("Number of Steps","steps",0,10.0,1.0,"Steps","Each step increases side length by a factor of sqrt(3)");
-	echoSlider("Line Width","lineWidth",0,20.0,5.0,"Pixels");
-	echoSlider("Line Fade","lineFade",0,20.0,5.0,"Pixels");
-	echoSlider("Gamma","gamma",0.1,4.0,10.0,"","Determines whether contrast is more visible in darker or brighter areas");
-	echoSlider("Blur Radius","gaussianBlur",0,10,1.0,"Pixels","Convolves the source image with a gaussian blur kernel with the given radius in both dimensions");
-?>
+		echoSlider("Minimum Side Length","minSize",0.1,50.0,10.0,"Pixels");
+		echoSlider("Number of Steps","steps",0,10.0,1.0,"Steps","Each step increases side length by a factor of sqrt(3)");
+		echoSlider("Line Width","lineWidth",0,20.0,5.0,"Pixels");
+		echoSlider("Line Fade","lineFade",0,20.0,5.0,"Pixels");
+		echoSlider("Gamma","gamma",0.1,4.0,10.0,"","Determines whether contrast is more visible in darker or brighter areas");
+		echoSlider("Blur Radius","gaussianBlur",0,10,1.0,"Pixels","Convolves the source image with a gaussian blur kernel with the given radius in both dimensions");
+	?>
 
-<div class="inputcontainer">
-    <div class="inputlabel">Color 0</div>
-    <select class="inputselect" name="color0" id="color0">
-        <option value=0>Black</option>
-        <option value=1>White</option>
-        <option value=2>Source Color</option>
-		<option value=3>Original Color (no blur)</option>
-    </select>
-	
-</div>
-<div class="inputcontainer">
-    <div class="inputlabel">Color 1</div>
-    <select class="inputselect" name="color1" id="color1">
-        <option value=0>Black</option>
-        <option value=1>White</option>
-        <option value=2>Source Color</option>
-		<option value=3>Original Color (no blur)</option>
-    </select>
-	
-</div>
-<div class="inputcontainer">
-    <div class="inputlabel">Greyscale Calc</div>
-    <select class="inputselect" name="greyscale" id="control">
-        <option value=0>Luminance</option>
-        <option value=1>Brightness</option>
-    </select>
-	<div class="tooltip">?<span class="tooltiptext">Luminance: <a href="https://en.wikipedia.org/wiki/Relative_luminance" target="_blank">sRGB relative luminance</a><br>Brightness: RGB weighted equally</span></div>
-</div>
-<div class="inputcontainer">
-    <div class="inputlabel">Invert greyscale input</div>
-    <input type="checkbox" id="invert">
-	<div class="tooltip">?<span class="tooltiptext">Default: Dark=High Density Hexagons<br>Inverted: Light=High Density Hexagons<br>Swapping Color 0 and Color 1 is recommended when this is checked</span></div>
-</div>
-<div class="inputcontainer">
-    <div class="inputlabel">Refresh on parameter change</div>
-    <input type="checkbox" id="refreshToggle">
-	<div class="tooltip">?<span class="tooltiptext">Recommend you uncheck this if you have limited graphics capabilities, or if you're using a large image.</span></div>
-    
-</div>
-<div class="inputcontainer">
-	<div style="text-align:center">
-	<button id="refreshButton">Refresh</button>
-    <button onclick="resetToDefaults()">Reset to Defaults</button>
-	<button onclick="saveImage()">Save Image</button>
+	<div class="inputcontainer">
+		<div class="inputlabel">Color 0</div>
+		<select class="inputselect" name="color0" id="color0">
+			<option value=0>Black</option>
+			<option value=1>White</option>
+			<option value=2>Source Color</option>
+			<option value=3>Original Color (no blur)</option>
+		</select>
+		
+	</div>
+	<div class="inputcontainer">
+		<div class="inputlabel">Color 1</div>
+		<select class="inputselect" name="color1" id="color1">
+			<option value=0>Black</option>
+			<option value=1>White</option>
+			<option value=2>Source Color</option>
+			<option value=3>Original Color (no blur)</option>
+		</select>
+		
+	</div>
+	<div class="inputcontainer">
+		<div class="inputlabel">Greyscale Calc</div>
+		<select class="inputselect" name="greyscale" id="control">
+			<option value=0>Luminance</option>
+			<option value=1>Brightness</option>
+		</select>
+		<div class="tooltip">?<span class="tooltiptext">Luminance: <a href="https://en.wikipedia.org/wiki/Relative_luminance" target="_blank">sRGB relative luminance</a><br>Brightness: RGB weighted equally</span></div>
+	</div>
+	<div class="inputcontainer">
+		<div class="inputlabel">Invert greyscale input</div>
+		<input type="checkbox" id="invert">
+		<div class="tooltip">?<span class="tooltiptext">Default: Dark=High Density Hexagons<br>Inverted: Light=High Density Hexagons<br>Swapping Color 0 and Color 1 is recommended when this is checked</span></div>
+	</div>
+	<div class="inputcontainer">
+		<div class="inputlabel">Refresh on parameter change</div>
+		<input type="checkbox" id="refreshToggle">
+		<div class="tooltip">?<span class="tooltiptext">Recommend you uncheck this if you have limited graphics capabilities, or if you're using a large image.</span></div>
+		
+	</div>
+	<div class="inputcontainer">
+		<div style="text-align:center">
+		<button id="refreshButton">Refresh</button>
+		<button onclick="resetToDefaults()">Reset to Defaults</button>
+		<button onclick="saveImage()">Save Image</button>
+		</div>
 	</div>
 </div>
 </body>
--- posts/symbio-ink-waltz.php
+++ posts/symbio-ink-waltz.php
@@ -1,3 +1,6 @@
-<div class="video">
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/7DyNqgY2tkw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<div class="postcontainer">
+	<p>This is a music video I made with my wonderful partner, Liz. She painted a few textures and I wrote a shader and a little tune to go with it as I modified parameters throughout the video. We were pretty happy with how it turned out as a first little foray into trying to do something like this.</p>
+	<div class="video">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/7DyNqgY2tkw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
 </div>
