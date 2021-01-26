1eda557e1ba4cbd44cf5b974bf5cc729006d55f0
Trevor Swope <trevor.m.swope@gmail.com>
2021-01-25 22:13:58 -0800

    update about page

--- html/about.php
+++ html/about.php
@@ -3,7 +3,10 @@ $page_name = "About";
 include_once($_SERVER['DOCUMENT_ROOT']."/../include/header.php");
 ?>
 
-<p>This is my website!</p>
+<div class="postcontainer">
+<h3 class="text-center">About</h3>
+<p>I am an experimental potato-based lifeform who escaped from the laboratory I was grown in. Somehow that led to me making small animations and playing some piano from time to time, the results of which I will sometimes post to this website. Has science gone too far? You tell me.</p>
+</div>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT']."/../include/footer.php");
