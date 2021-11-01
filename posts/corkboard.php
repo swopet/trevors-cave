866e78d2428f92f33b2ebd36debdffc0ae37922c
Trevor Swope <pcllab@dmcinfo.com>
2021-11-01 09:49:05 -0700

    add Corkboard woops

---
+++ posts/corkboard.php
@@ -0,0 +1,4 @@
+<div class="postcontainer">
+	<p>My brother (Carson Swope) and I collaborated to participate in the Game Maker's Toolkit Game Jam 2021, and made a little puzzle game called Corkboard in 48 hours. Music was made by Madison Butler a.k.a. Wahx (also in 48 hours!!!); check out more of his stuff <a href = https://open.spotify.com/album/0Kc34LaMr35wC4iXvl3ocu?si=onwm0gLcStyrAnCwNusG-Q>here</a>.</p>
+	<iframe src="https://itch.io/embed/1081717" height="167" width="552" frameborder="0"><a href="https://revort.itch.io/corkboard">Corkboard by revort</a></iframe>
+</div>
